52c62f190a980d94d59dcba735f7aeb9587a8e57
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-28 12:03:18 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,12 @@
 resource "aws_instance" "example" {
   ami           = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
-  subnet_id = "subnet-1c1e0466"
+  subnet_id = [aws_subnet.example.id]
   vpc_security_group_ids = [aws_security_group.allow_22_all.id]
   key_name = "terraform-keypair"
   tags = {
     Name = "demo-ansible-instance"
     Purpose = "Terraform Enterprise test"
-    inte = "infracost"
   }
    connection {
      user = var.ssh_user # default= "ec2-user"
