abe8d5ff60c633972650bb4b6c2437ed11376583
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-05-04 11:44:26 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,3 +6,8 @@ module "vpc" {
   source = "github.com/jhp0204/terraform-aws-simple/vpc/"
 }
 
+  ## module 변경 테스트 시, 사용
+# module "vpc" {
+#   source = "github.com/jhp0204/terraform-aws-vpc"
+# }
+  
