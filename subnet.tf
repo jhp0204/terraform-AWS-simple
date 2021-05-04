6bebdcaaf8a4f2a489308b106fb690460159e67f
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-05-04 11:31:29 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "example" {
   vpc_id = module.vpc.vpc_id 
   availability_zone = "us-east-1"
   tags = {
-    Name = "example-subnet"
+    Name = "example-subnet",
+    propose = "terraform-presentation"
   }
 }
