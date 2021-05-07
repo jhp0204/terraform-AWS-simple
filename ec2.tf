31096bfc6b8b591edfe41317443e3f8f5cb7f8e3
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-05-07 14:45:04 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
-  subnet_id = [aws_subnet.example.id]
+  subnet_id = aws_subnet.example.id
   vpc_security_group_ids = [aws_security_group.allow_22_all.id]
   key_name = "terraform-keypair"
   tags = {
