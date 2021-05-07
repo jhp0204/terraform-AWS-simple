ce4d860c8a3705c079545ff462e697f0b82315ab
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-05-07 15:04:01 +0900

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,21 +1,21 @@
-resource "aws_security_group" "allow_22_all" {
-  name = "allow_22_all"
-  description = "allow ssh port from all"
-  vpc_id = module.vpc.vpc_id
-  ingress {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-  }
-  egress {
-    cidr_blocks = [ "0.0.0.0/0"]
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-  }
-  tags = {
-    Name =  "allow_22_all"
-    Purpose = "terraform-presentation"
-  }
-}
+#resource "aws_security_group" "allow_22_all" {
+#  name = "allow_22_all"
+#  description = "allow ssh port from all"
+#  vpc_id = module.vpc.vpc_id
+#  ingress {
+#    cidr_blocks = [ "0.0.0.0/0" ]
+#    from_port = 22
+#    to_port = 22
+#    protocol = "tcp"
+#  }
+#  egress {
+#    cidr_blocks = [ "0.0.0.0/0"]
+#    from_port = 0
+#    to_port = 0
+#    protocol = "-1"
+#  }
+#  tags = {
+#    Name =  "allow_22_all"
+#    Purpose = "terraform-presentation"
+#  }
+#}
