4d7602f98d235e02ff9b402e438ee16908a162db
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-05-07 15:00:50 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,9 +1,9 @@
-resource "aws_subnet" "example" {
-  vpc_id = module.vpc.vpc_id
-  cidr_block ="0.0.0.0/20"     
+#resource "aws_subnet" "example" {
+#  vpc_id = module.vpc.vpc_id
+#  cidr_block ="0.0.0.0/20"     
 #  availability_zone = "us-east-2"
-  tags = {
-    Name = "example-subnet"
-    Purpose = "terraform-presentation"
-  }
-}
+#  tags = {
+#    Name = "example-subnet"
+#    Purpose = "terraform-presentation"
+#  }
+#}
