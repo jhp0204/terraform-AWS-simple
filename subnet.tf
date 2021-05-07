7e617904430a90e248ee4a2515c970710e357b81
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-05-07 14:51:59 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "example" {
   vpc_id = module.vpc.vpc_id
-  cidr_block ="172.31.64.0/20"     
+  cidr_block ="172.31.80.0/20"     
 #  availability_zone = "us-east-2"
   tags = {
     Name = "example-subnet"
