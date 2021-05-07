b3ccead29f939301fef3deae3ca1b5d8f0553094
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-05-07 15:04:21 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-module "vpc" {
-  source = "github.com/jhp0204/terraform-aws-simple/vpc/"
-}
+#module "vpc" {
+#  source = "github.com/jhp0204/terraform-aws-simple/vpc/"
+#}
 
   ## module 변경 테스트 시, 사용
 # module "vpc" {
