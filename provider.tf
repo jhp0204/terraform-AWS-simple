3f7ffa988257897f86308eb3be0136a873da0be5
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-11-22 11:16:17 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,10 +11,16 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
-#TEST AFTER TERRAFORM PROVIDER CHANGED
+
+#SSH TEST
 module "vpc" {
- source = "github.com/jhp0204/terraform-aws-simple/vpc/"
+ source = "git@github.com:jhp0204/terraform-aws-simple/vpc.git"
 }
+
+# #TEST AFTER TERRAFORM PROVIDER CHANGED
+# module "vpc" {
+#  source = "github.com/jhp0204/terraform-aws-simple/vpc/"
+# }
 
   ## module 변경 테스트 시, 사용
 # module "vpc" {
